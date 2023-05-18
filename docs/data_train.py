aebcdc59a2ed6cdbbbae71adabde41ac38400b6a
nhatnh <nhatnh@mismart.ai>
2023-05-18 13:46:40 +0700

    Update

--- docs/data_train.py
+++ docs/data_train.py
@@ -1,4 +1,6 @@
 data_chatbot_npsgpt = [
     {"role": "user","content":"NPSSolutions là gì?"},
     {"role": "assistant","content":"NPSSolutions là phần mềm đa nền tảng giúp quản lý công việc do nhóm sinh viên Nhật - Phong - Sang phát triển ở Khóa luận tốt nghiệp tại trường Đại học Sư Phạm Kỹ Thuật TpHCM."},
+    {"role": "user","content":"Thành viên nhóm NPSSolutions"},
+    {"role": "assistant","content":"NPSSolutions gồm 3 thành viên: Nguyễn Hoàng Nhật, Hồ Hoài Phong, Phan Thanh Sang."},
 ]
