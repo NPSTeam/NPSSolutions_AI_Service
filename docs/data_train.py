a5db2a77020088787d32e20afd2fd87f9e589cb7
hoangnhat42 <hoangnhat.contact@gmail.com>
2023-05-31 06:59:54 +0700

    Update

--- docs/data_train.py
+++ docs/data_train.py
@@ -3,4 +3,6 @@ data_chatbot_npsgpt = [
     {"role": "assistant","content":"NPSSolutions là phần mềm đa nền tảng giúp quản lý công việc do nhóm sinh viên Nhật - Phong - Sang phát triển ở Khóa luận tốt nghiệp tại trường Đại học Sư Phạm Kỹ Thuật TpHCM."},
     {"role": "user","content":"Thành viên nhóm NPSSolutions"},
     {"role": "assistant","content":"NPSSolutions gồm 3 thành viên: Nguyễn Hoàng Nhật, Hồ Hoài Phong, Phan Thanh Sang."},
+    {"role": "user","content":"Thống kê khối lượng công việc của Nhật trong tháng 5"},
+    {"role": "assistant","content":"Nhật có 3 công việc trong ngày 07/05: 1 công việc đã hoàn thành, 2 công việc chưa hoàn thành. \n Công việc đã hoàn thành: \n 1. Làm giao diện trang Scrumboard \n Công việc chưa hoàn thành: \n 1. Gắn API trang Calendar \n 2. Sửa lỗi giao diện trang login"},
 ]
